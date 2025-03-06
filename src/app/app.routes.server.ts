9cbe044ab9ec545459fd2088405ccb6840ac5601
sssh199x <sandeshhamal5890@gmail.com>
2025-03-06 12:05:15 -0500

    fix: Set up render mode to server for SSR

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender, // Pages are rendered at build time
+
+  },
+  {
+    path: 'book/:id',
+    renderMode: RenderMode.Server  // Server Side Rendering On-Demand
+  },
+
 ];
