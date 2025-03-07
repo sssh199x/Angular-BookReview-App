f6f00a7ab2d1065059a4b56fab38f44ae265a7da
sssh199x <sandeshhamal5890@gmail.com>
2025-03-07 12:07:05 -0500

    add: error-page-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import {RouterModule,} from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {NavigationEnd, Router, RouterModule,} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,29 @@ import {HeaderComponent} from './header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'book-review-app';
+  isErrorPage = false;
+
+  private router: Router = inject(Router);
+
+  ngOnInit() {
+    // Check on init for current route
+    this.checkIfErrorPage(this.router.url);
+
+    //Listen for route changes
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+        this.checkIfErrorPage(event.url);
+      }
+    );
+  }
+
+  checkIfErrorPage(url: string) {
+    // Check if current route matches any error route pattern
+    this.isErrorPage = url.includes('error-page') || url === '/404';
+  }
+
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import {Routes} from '@angular/router';
 import {BookListComponent} from './book-list/book-list.component';
 import {BookDetailsComponent} from './book-details/book-details.component';
 import {ReviewFormComponent} from './review-form/review-form.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
 
 export const routes: Routes = [
   {path: '', component: BookListComponent, title: 'Book List'},
   {path: 'book/:id', component: BookDetailsComponent, title: 'Book Details'},
   {path: 'review/:id', component: ReviewFormComponent, title: 'Review Details'},
+  // Explicit error page route
+  { path: 'error-page', component: ErrorPageComponent, title: 'Error Page' },
+
+
+  // Wildcard route for 404 - must be the last route!
+  {path: '**' ,redirectTo: 'error-page'},
 ];
 
